d0f1ab1ee7b202aed86b57da2edfc00b78d815a0
Andrey Smirnov <av_smirnov@betboom.org>
2025-04-12 17:45:57 +0300

    feat(*): add link to home btn

--- src/components/layouts/BarBottom/index.tsx
+++ src/components/layouts/BarBottom/index.tsx
@@ -1,4 +1,5 @@
 import { Flex } from '@chakra-ui/react';
+import { Link } from 'react-router';
 
 import { ButtonBar } from '~/components/ui-kit/buttons/ButtonBar';
 import { HomeIcon } from '~/components/ui-kit/Icons';
@@ -9,7 +10,17 @@ import avatar from './assets/avatar.png';
 
 export const BarBottom = () => (
     <Flex h='100%'>
-        <ButtonBar icon={HomeIcon} label='Главная' active />
+        <Link
+            to='/'
+            style={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <ButtonBar icon={HomeIcon} label='Главная' active />
+        </Link>
         <ButtonBar icon={SearchIcon} label='Поиск' />
         <ButtonBar icon={WriteIcon} label='Записать' />
         <ButtonBar imageSrc={avatar} label='Мой профиль' />
